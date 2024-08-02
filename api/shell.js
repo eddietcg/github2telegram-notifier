9839aae504eeb18fa7de43d10e046813597ca07a
eddie.y.tcg <eddie.y.tcg@gmail.com>
2024-08-03 01:50:12 +0800

    Create shell.js

---
+++ api/shell.js
@@ -0,0 +1,33 @@
+const event = require("../lib/eventSwitch.js");
+const sendMsg = require("../lib/sendMsg.js");
+const verifyWebhook = require("../lib/verifyWebhook.js");
+
+// The main, exported, function of the endpoint,
+// dealing with the request and subsequent response
+module.exports = async (req, res) => {
+    const body = req.body;
+    const method = req.method;
+    const gh_event = req.headers["x-github-event"];
+    const gh_sig = req.headers["x-hub-signature"];
+    const gh_sig_256 = req.headers["x-hub-signature-256"];
+
+    if ((method === "POST") && verifyWebhook.verifyWebhook(body, gh_sig_256)) {
+        event.eventSwitch(gh_event, body).then((Msg) => {
+            console.log("msg=", Msg);
+            if (!Msg) {
+                console.warn("msg is blank, skip send message");
+                return res.status(201).send({ status: "ok" });
+            }
+            sendMsg.sendMsg(Msg, "-1001732184955")
+                .then(() => {
+                    res.status(201).send({ status: "ok" });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(err.response.status).send(err.response.statusText);
+                });
+        });
+    } else {
+        res.status(403).send();
+    }
+};
